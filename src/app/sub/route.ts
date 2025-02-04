2c2afc6c01e45fbebe0493b56787821d6b3c782c
xqd <xqd922@gmail.com>
2025-02-04 21:25:55 +0800

    最新备份

--- src/app/sub/route.ts
+++ src/app/sub/route.ts
@@ -216,7 +216,10 @@ export async function GET(request: Request) {
         {
           name: 'HK',
           type: 'url-test',
-          proxies: proxies.filter(p => /🇭🇰|香港|HK|Hong Kong|HKG/.test(p.name) && !/家宽|Home/.test(p.name)).map(p => p.name),
+          proxies: (() => {
+            const filtered = proxies.filter(p => /🇭🇰|香港|HK|Hong Kong|HKG/.test(p.name) && !/家宽|Home/.test(p.name)).map(p => p.name)
+            return filtered.length > 0 ? filtered : ['DIRECT']
+          })(),
           url: 'http://www.gstatic.com/generate_204',
           interval: 300,
           tolerance: 50
