68730ac765c41d1dad90e81b19cb58c3a00bc073
xqd <xqd922@gmail.com>
2025-08-31 01:23:40 +0800

    日志为error

--- src/config/singbox.ts
+++ src/config/singbox.ts
@@ -12,7 +12,7 @@ export function generateSingboxConfig(proxies: Proxy[], shouldFormatNames: boole
 
   return {
     log: {
-      level: "info"
+      level: "error"
     },
     dns: {
       servers: [
