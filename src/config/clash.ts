1f24c398a2170d2363dce77d3df1a926e020d223
seven <xqdwmq@gmail.com>
2026-07-13 14:31:05 +0800

    chore: remove unused domain rules from clash config

--- src/config/clash.ts
+++ src/config/clash.ts
@@ -320,15 +320,12 @@ export const defaultConfig: ClashConfig = {
 
     'DOMAIN,sub.xqd.pp.ua,DIRECT',
     'DOMAIN,1001.pp.ua,DIRECT',
-    'DOMAIN,cdn.lilyemby.com,DIRECT',
     'DOMAIN-SUFFIX,gegeselect.hk,DIRECT',
-    'DOMAIN-SUFFIX,qwen.ai,DIRECT',
     'DOMAIN,stream-1.sadchicktv.com,DIRECT',
     'DOMAIN-SUFFIX,neko.mo.cn,Manual',
     'IP-CIDR,95.161.76.100/31,REJECT,no-resolve',
     'DOMAIN-SUFFIX,steamcontent.com,DIRECT',
     'DOMAIN,msmp.abchina.com.cn,REJECT',
-    'DOMAIN-SUFFIX,apple-cdn.net,DIRECT',
     'DOMAIN-SUFFIX,sharepoint.com,DIRECT',
 
     'RULE-SET,emby,Emby',
